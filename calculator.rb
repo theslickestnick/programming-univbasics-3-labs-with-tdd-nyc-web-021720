c1edc0d4e51f50f2f2aa75e0b01a2288754d7b2e
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-21 15:44:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
 first_number = 1
 second_number = 2
 sum = first_number + second_number
+difference = second_number - first_number
 
 # do get_number(number)
 #   first_number = get_variable_from_file
