594688a599c79bdd311968b2461f3d65859dcbb0
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-21 15:46:28 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
-# Add your variables here
+first_number = 1
+second_number = 2
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+# do get_number(number)
+#   first_number = get_variable_from_file
+# end
+
+ # Add your variables here
