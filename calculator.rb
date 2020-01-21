b54b18a402596d463b444c4211d117c53add07ed
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-21 15:45:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,7 @@ second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
 
 # do get_number(number)
 #   first_number = get_variable_from_file
